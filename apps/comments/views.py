5e174d0faec9e6b72b7ff7fd46edaf2a81e62ac3
glebuzheg <puper.inferno@yandex.ru>
2019-02-10 23:54:16 +0300

    update comment list modal

--- apps/comments/views.py
+++ apps/comments/views.py
@@ -1,26 +1,32 @@
+from django.forms import model_to_dict
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
-from django.urls import reverse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import CreateView
 
 from apps.images.models import Image
 from .forms import CommentForm
-from .models import Comment
 
 
+@method_decorator(csrf_exempt, name='dispatch')
 class CommentCreateView(CreateView):
-    template_name = 'comments/comment_create.html'
-    model = Comment
-    form_class = CommentForm
+    def post(self, request, *args, **kwargs):
+        data = dict()
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            self.image = self._get_image(kwargs.get('image_id'))
+            form.instance.image = self.image
+            comment = form.save()
+            data['comment'] = model_to_dict(comment)
+        else:
+            data['error'] = "form not valid!"
+        return JsonResponse(data)
 
-    def get_success_url(self):
-        return reverse('image_list')
-
-    def get_context_data(self, **kwargs):
-        self.image = get_object_or_404(Image, id=self.kwargs['image_id'])
-        kwargs['image'] = self.image
-        return super().get_context_data(**kwargs)
+    def _get_image(self, id):
+        return get_object_or_404(Image, id=id)
 
     def form_valid(self, form):
         self.image = get_object_or_404(Image, id=self.kwargs['image_id'])
-        form.instance.image = self.image
+        form.instance.image_id = self.image.id
         return super().form_valid(form)
